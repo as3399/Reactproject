e064939597e3e8870c3a44ce4f2ebcaac6fed468
Angad Singh <angadsingh3399@gmail.com>
2020-12-31 13:38:36 +0530

    ver 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
 import React from 'react'
 import Sdata from './Sdata'
 import Card from './Card'
+import Foot from './Foot'
 
 function App() {
     return (
         <div>
-            <h1 className="navbar_title">List of Top 5 Web Series 2020</h1>
-
-            <Card
-                poster={Sdata[0].poster}
-                sname={Sdata[0].sname}
-                link={Sdata[0].link}
-                title={Sdata[0].title}
-            />
-            <Card
-                poster={Sdata[1].poster}
-                sname={Sdata[1].sname}
-                link={Sdata[1].link}
-                title={Sdata[1].title}
-            />
-            <Card
-                poster={Sdata[2].poster}
-                sname={Sdata[2].sname}
-                link={Sdata[2].link}
-                title={Sdata[2].title} />
-            <Card
-                poster={Sdata[3].poster}
-                sname={Sdata[3].sname}
-                link={Sdata[3].link}
-                title={Sdata[3].title}
-            />
-            <Card
+        <div className="navbar">
            
-                poster={Sdata[4].poster}
-                sname={Sdata[4].sname}
-                link={Sdata[4].link}
-                title={Sdata[4].title}
-            />
+            <h1>List of Top 5 Web Series 2020</h1>
+                     
+                    
         </div>
-    );
+                
+        
+       
+        {Sdata.map((val, index) => {
+            console.log(index);
+            return (
+                <Card
+                    key={val.id}
+                    poster={val.poster}
+                    sname={val.sname}
+                    link={val.link}
+                    title={val.title}
+                />
+            )
+        }
+        )}
+        <Foot />
+        </div>
+        
+        
+    )
 }
+
 export default App;
---
+++ src/Foot.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Foot() {
+    return <div className= "footer">
+        <p>Copyright. All Rights Reserved.</p>
+    </div>
+}
+export default Foot;
